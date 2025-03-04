963464c4e94f667b4415a2c9aeae69c518df0303
Gianluca <studio.nagcas@outlook.it>
2025-03-04 22:32:23 +0100

    add fetch async await

---
+++ src/services/useFetchAW.jsx
@@ -0,0 +1,48 @@
+import { useEffect, useState } from 'react';
+
+export function useFetchAW(url) {
+  const [loading, setLoading] = useState(false);
+  const [data, setData] = useState(null);
+  const [error, setError] = useState(null);
+
+  // Funzione asincrona per fare la richiesta all'API
+  useEffect(() => {
+    // Verifica se l'url è vuoto
+    if (!url) return;
+
+    // Loading di attesa impostato a true
+    setLoading(true);
+    const fetchData = async () => {
+      try {
+        // URL dell'API di esempio
+        const response = await fetch(url);
+        
+        console.log(response);
+        
+        // Verifica se la risposra è 200 (status HTTP 200)
+        if (!response.ok) {
+          throw new Error('Errore nella richiesta.');
+        }
+
+        // Convertiamo la risposta in formato JSON
+        const data = await response.json();
+        
+        // Mostra il risultato
+        console.log(data);
+        setData(data);
+      } catch (error) {
+        // Gestione degli errori      
+        console.error('Errore', error);
+        setError(error.message);
+      } finally {
+        setLoading(false);
+      }
+    };
+
+    // Chiamata alla funzione asincrona
+    fetchData();
+
+  }, [url]);
+
+  return { data, error, loading };
+} 
