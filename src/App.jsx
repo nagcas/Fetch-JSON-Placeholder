7dc5083fca93dcde5f659a4ad7cded661ead50a7
Gianluca <studio.nagcas@outlook.it>
2025-03-05 00:15:44 +0100

    add route detail users and context

--- src/App.jsx
+++ src/App.jsx
@@ -6,17 +6,22 @@ import Home from './components/pages/home/Home';
 import ListPosts from './components/listPosts/ListPosts';
 import Photos from './components/photos/Photos';
 import Albums from './components/albums/Albums';
+import DetailUsers from './components/pages/detailUsers/DetailUsers';
+import { UsersContextProvider } from './context/UsersContext';
 
 function App() {
 
   return (
     <>
       <div>
-        <Route component={Home} path='/' />
-        <Route component={ListUsers} path='/users' />
-        <Route component={ListPosts} path='/posts' />
-        <Route component={Photos} path='/photos' />
-        <Route component={Albums} path='/albums' />
+        <UsersContextProvider>
+          <Route component={Home} path='/' />
+          <Route component={ListUsers} path='/users' />
+          <Route component={ListPosts} path='/posts' />
+          <Route component={Photos} path='/photos' />
+          <Route component={Albums} path='/albums' />
+          <Route component={DetailUsers} path='/users/:id' />
+        </UsersContextProvider>
       </div>
     </>
   )
