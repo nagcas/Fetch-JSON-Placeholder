ed4d8c43ce2178760d9a46ffca8b44829d161b04
Gianluca <studio.nagcas@outlook.it>
2025-03-04 16:17:43 +0100

    add albums

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,8 @@ import { Route } from 'wouter';
 import ListUsers from './components/listUsers/ListUsers';
 import Home from './components/pages/home/Home';
 import ListPosts from './components/listPosts/ListPosts';
+import Photos from './components/photos/Photos';
+import Albums from './components/albums/Albums';
 
 function App() {
 
@@ -13,6 +15,8 @@ function App() {
         <Route component={Home} path='/' />
         <Route component={ListUsers} path='/users' />
         <Route component={ListPosts} path='/posts' />
+        <Route component={Photos} path='/photos' />
+        <Route component={Albums} path='/albums' />
       </div>
     </>
   )
---
+++ src/components/albums/Albums.jsx
@@ -0,0 +1,27 @@
+import './Albums.css';
+import { useFetch } from '../../services/useFetch';
+import Errors from '../errors/Errors';
+import Loading from '../loading/Loading';
+import ReturnHome from '../returnHome/ReturnHome';
+
+const apiURL = 'https://jsonplaceholder.typicode.com/albums';
+
+export default function Albums() {
+
+  const { data, error, loading } = useFetch(apiURL);
+
+  return (
+    <>
+      <h1>Albums</h1>
+      <Errors error={error} />
+      <Loading loading={loading} />
+      {data?.map((item) => (
+        <div key={item.id} className='albums'>
+          <small>id: {item.id}</small>
+          <h2>Title: {item.title}</h2>
+        </div>    
+      ))}
+      <ReturnHome />
+    </>
+  )
+}
